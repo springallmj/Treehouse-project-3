2646d406163ae16d0fb72617a84df9bd27b03c4c
springallmj <springallmj@gmail.com>
2021-08-17 10:04:32 +1000

    Basic Info fixed
    Dynamic validation on name field only - fixed.
    email to be designed in to same loop.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //initialise focus on name field
-const nameField = document.getElementById('name');
-nameField.focus();
+const userName = document.getElementById('name');
+userName.focus();
 
 //Display 'other job' description ONLY if other job is selected as job roll
 const jobRoll=document.getElementById('title');
@@ -96,48 +96,94 @@ const zipCode = document.getElementById('user-zip');
 const cvv = document.getElementById('user-cvv');
 const cardPayment = document.getElementById('credit-card');
 
+const basicInfo=document.getElementById('basic-info');
+const activities=document.getElementById('activities');
+const paymentInfo=document.getElementById('payment-info');
+
 //Submit validation and error handling
 const form=document.getElementById("registration");
 
 form.addEventListener('submit', (e) => {
-    const requiredFields = document.querySelectorAll("[testName]");
-    //loops all require fields and applies the appropriate validation and
-    //event listeners to dynamically update fields to 'valid' when complete
-    for(let i=0; i<requiredFields.length; i++){
-        const field=requiredFields[i];
-        const testName=field.getAttribute('testName'); 
+    //const requiredFields = document.querySelectorAll("[testName]");
+    const requiredSections=[basicInfo, activities, paymentInfo];    
 
-        if(testName!='select' && testName!='checkBox'){   
-            const valid=validators[testName](field.value);
-            if(!valid){
-                event.preventDefault();
-            }
-            notValidError(valid, field);
-            hintDisplay(valid, field.nextElementSibling, testName);
-            field.addEventListener('keyup', createErrorListener());
-            field.addEventListener('keyup', createHintListener(hintDisplay));
-        }
-        //'Select' Field validation - credit card details cvv and exp. date.
-        //a seecltion must be made.
-        else if(testName=='select'){
-            const valid=validators[testName](field.value);
-            if(!valid){
-                event.preventDefault();
-                notValidError(valid, field);
-                field.addEventListener('change', createErrorListener());
-            }
-        }
-        //Checkbox options validated - at least one selection must be made
-        else if(testName=='checkBox'){
-            const valid=checkBoxes();
-            if(!valid){
-                event.preventDefault();
-                hintDisplay(valid, field.parentElement.lastElementChild);   
-                field.addEventListener('change', createHintListener(hintDisplay));
-            }
+    event.preventDefault();
+    // function notValid (){
+
+    // }
+
+    // function textValidator(){
+
+    // }
+    function createTextListener(validator, element){
+        return e => {
+          const text = e.target.value;
+          notValidError(validator(text), element);
         }
     }
+
+    //use required fields to cycle through multiple validators
+    for(let i=0; i<1; i++){
+
+        const fieldSet=requiredSections[i];
+        const requiredFields=fieldSet.querySelectorAll("[required]");
+
+        for(let j=0; j<1; j++){
+            const field=requiredFields[j];
+            const fieldId=field.id;
+            const validator=validators[fieldId];
+            const show=validator(field.value);
+            notValidError(show, fieldSet.firstElementChild);
+            field.addEventListener("keyup", createTextListener(validator, fieldSet.firstElementChild));
+        }
+        
+
+        
+        
+    }
+
+
+    // for(let i=0; i<requiredFields.length; i++){
+    //     const field=requiredFields[i];
+    //     const testName=field.getAttribute('testName'); 
+
+    //     if(testName!='select' && testName!='checkBox'){   
+    //         const valid=validators[testName](field.value);
+    //         if(!valid){
+    //             event.preventDefault();
+    //         }
+    //         notValidError(valid, field);
+    //         hintDisplay(valid, field.nextElementSibling, testName);
+    //         field.addEventListener('keyup', createErrorListener());
+    //         field.addEventListener('keyup', createHintListener(hintDisplay));
+    //     }
+    //     //'Select' Field validation - credit card details cvv and exp. date.
+    //     //a seecltion must be made.
+    //     else if(testName=='select'){
+    //         const valid=validators[testName](field.value);
+    //         if(!valid){
+    //             event.preventDefault();
+    //             notValidError(valid, field);
+    //             field.parentElement.addEventListener('change', createErrorListener());
+    //         }
+    //     }
+    //     //Checkbox options validated - at least one selection must be made
+    //     else if(testName=='checkBox'){
+    //         const valid=checkBoxes();
+    //         if(!valid){
+    //             event.preventDefault();
+    //             hintDisplay(valid, field.parentElement.lastElementChild);   
+    //             field.addEventListener('change', (e) =>{
+    //                 const field = event.target
+    //                 const element = document.getElementById("activities-hint");
+    //                 const valid = validators.checkBox();
+    //                 hintDisplay(valid, element);
+    //             });
+    //         }
+    //     }
+    // }
 });
+
 
 
 //Field Validation functions
@@ -157,7 +203,7 @@ const validators = {
     emailHint: function (email){
 
     },
-    userName: function (text){
+    name: function (text){
         if (text)
             return true;
         else
@@ -189,7 +235,7 @@ function createHintListener(display) {
         const valid = validators[fieldName](fieldValue);
         display(valid, element, fieldName, fieldValue);
     }
-  };
+};
 
 function createErrorListener(){
     return (e) => {
@@ -222,29 +268,38 @@ function hintDisplay(valid, element, fieldName, fieldValue){
 //NOTE it is assumed that a cvv and exp date are required for cc details.
 //not-valid has been applied to these fields
 function notValidError(show, element){
-    if(element.getAttribute('testName')=='select' ||
-       element.getAttribute('testName')=='checkBox'){
-        if(show){
-            element.className="valid"
-        }else{
-            element.className="not-valid"
-        }
-    }else{
-        if(show){
+    if(show){
         element.className="valid";
-        element.parentElement.className="valid";
-      } else{
+    }else{
         element.className="not-valid";
-        element.parentElement.className="not-valid";
-      }
     }
 }
+
+
+
+
+    // if(element.getAttribute('testName')=='select' ||
+    //    element.getAttribute('testName')=='checkBox'){
+    //     if(show){
+    //         element.className="valid"
+    //     }else{
+    //         element.className="not-valid"
+    //     }
+    // }else{
+    //     if(show){
+    //     element.className="valid";
+    //     element.parentElement.className="valid";
+    //   } else{
+    //     element.className="not-valid";
+    //     element.parentElement.className="not-valid";
+    //   }
+    // }}
 
 function checkBoxes(){
     for(let i=0; i<courses.length; i++){
         if(courses[i].checked){
             return true;
         }
-    return false;
     }
+    return false;
 }
